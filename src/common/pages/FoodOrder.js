3a4a60984f4ff01b45861ee1fc4528059b6556db
xuyuan <xuyuan@sendcloud.im>
2018-04-23 19:03:47 +0800

    socket init

--- src/common/pages/FoodOrder.js
+++ src/common/pages/FoodOrder.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-
+import socket from '../api/socket';
 class FoodOrder extends Component {
   state = {  }
+
+  componentWillMount() {
+    socket.on('', {
+
+    })
+  }
+
   render() {
     return (
       <div>foodOrder</div>
